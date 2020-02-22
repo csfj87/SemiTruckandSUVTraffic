a5f71ac07a1549c7537a71468dd48a96a3f24a03
csfj87 <60517816+csfj87@users.noreply.github.com>
2020-02-21 22:58:35 -0500

    Create mySemi.java
    Semi Truck Class.

---
+++ mySemi.java
@@ -0,0 +1,13 @@
+package com.company;
+//created a traffic light class that has getters and setters to update the traffic light as directed by the user.
+public class mySemi {
+    public String command;
+    public void setSemiCommand(String semiCommand)
+    {
+        this.command = semiCommand;
+    }
+    public String getSemiCommand()
+    {
+        return command;
+    }
+}
