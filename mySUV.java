a69eea01747af2fa79349997bdba3b79749c4534
csfj87 <60517816+csfj87@users.noreply.github.com>
2020-02-21 22:14:46 -0500

    Create mySUV.java
    Class for SUV.

---
+++ mySUV.java
@@ -0,0 +1,13 @@
+package com.company;
+//created an SUV class that has getters and setters to update the traffic light as directed by the user.
+public class mySUV {
+    public String suv;
+    public void setSUVCommand(String suvCommand)
+    {
+        this.suv = suvCommand;
+    }
+    public String getSUVCommand()
+    {
+        return suv;
+    }
+}
