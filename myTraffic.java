07c870b0f74c8413ed9b7516a9b69cef272cf9bf
csfj87 <60517816+csfj87@users.noreply.github.com>
2020-02-21 22:16:31 -0500

    Create myTraffic.java
    Traffic Light class.

---
+++ myTraffic.java
@@ -0,0 +1,13 @@
+package com.company;
+//created a traffic light class that has getters and setters to update the traffic light as directed by the user.
+public class myTraffic {
+    public String light;
+    public void setTrafficLight(String trafficLight)
+    {
+        this.light = trafficLight;
+    }
+    public String getTrafficLight()
+    {
+        return light;
+    }
+}
